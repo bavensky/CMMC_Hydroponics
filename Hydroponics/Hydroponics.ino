024cd8bd0c0b2a2b0ca27aadb14b118a08f3c167
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2015-03-19 21:44:28 +0700

    update

--- Hydroponics/Hydroponics.ino
+++ Hydroponics/Hydroponics.ino
@@ -1,7 +1,10 @@
-#include <DHT22.h>
+#include <Wire.h>
 #include <OneWire.h>
-#include <DallasTemperature.h>
 #include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT22.h>
+#include <DallasTemperature.h>
+#include "RTClib.h"
  
 // Data wire is plugged into pin 7 on the Arduino
 #define DHT22_PIN 7
@@ -11,6 +14,7 @@
 // Data wire is plugged RX into pin 3 and TX into pin 2 on the Arduino
 #define rx 2                  
 #define tx 3  
+
 // Setup a DHT22 instance
 DHT22 myDHT22(DHT22_PIN);
 
@@ -20,45 +24,78 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
+// LiquidCrystal I2C adress 0x27 
+LiquidCrystal_I2C lcd(0x27,  20,  4);
+
+// initial 
+RTC_DS1307 rtc;
+
 // initial soft serial 
 SoftwareSerial myserial(rx, tx); 
 char ph_data[20];
 byte received_from_sensor=0; 
 float ph=0;
 
+// custom characters
+byte water[8] = {
+  0b00000,
+  0b00100,
+  0b00100,
+  0b01110,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b01110
+};
+
+int i = 0;
 
 void setup(void)  {
   Serial.begin(9600);
   myserial.begin(9600);
   sensors.begin();
+  Wire.begin();
+  rtc.begin();
+  lcd.init(); 
+  lcd.backlight();
   
   pinMode(8, INPUT_PULLUP);
   pinMode(9, INPUT_PULLUP);
   pinMode(10, INPUT_PULLUP);
   pinMode(11, INPUT_PULLUP);
+  
+  lcd.createChar(0, water);
+  
+//  rtc.adjust(DateTime(__DATE__, __TIME__));
 }
  
  
 void loop(void)  {
   check_ph();
   check_dht22();
+  check_rtc();
   sensors.requestTemperatures(); 
-
-  Serial.print("Device is : ");
-  Serial.print("DS18B20 = ");
-  Serial.print(sensors.getTempCByIndex(0));
-  Serial.print("  Temperature = ");
-  Serial.print(myDHT22.getTemperatureC());
-  Serial.print("  Humidity = ");
-  Serial.print(myDHT22.getHumidity());
-  Serial.print("  pH = ");
-  Serial.print(ph);
   
-  Serial.print("  Button : ");
-  Serial.print(digitalRead(8));
-  Serial.print(digitalRead(9));
-  Serial.print(digitalRead(10));
-  Serial.println(digitalRead(11));
+  
+  lcd.home();
+  lcd.print("Hydropnics");
+  lcd.setCursor(0, 1);
+  lcd.write(0);
+//  lcd.print(sensors.getTempCByIndex(0));
+//  lcd.setCursor(0, 2);
+//  lcd.print("Temperature = ");
+//  lcd.print(myDHT22.getTemperatureC());
+//  lcd.setCursor(0, 3);
+//  lcd.print("Humidity = ");
+//  lcd.print(myDHT22.getHumidity());
+//  lcd.print("  pH = ");
+//  lcd.print(ph);
+  
+//  Serial.print("  Button : ");
+//  Serial.print(digitalRead(8));
+//  Serial.print(digitalRead(9));
+//  Serial.print(digitalRead(10));
+//  Serial.println(digitalRead(11));
   
 //  if(ph >= 3.8)  {
 //      Serial.println("OVER");
@@ -81,4 +118,21 @@ void check_ph()  {
     ph_data[received_from_sensor] = 0;
     ph = atof(ph_data);
   }
+}
+
+void check_rtc()  {
+    DateTime now = rtc.now();
+    
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
 }
